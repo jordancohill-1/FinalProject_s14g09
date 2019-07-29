d05a753773a5ca993acbb8156d02a4d11e75d696
jordancohill-1 <cohill_jordan@bah.com>
2019-07-29 17:27:43 -0600

    working jsontopos

--- jsonToPos.py
+++ jsonToPos.py
@@ -13,9 +13,8 @@ cur.execute('''CREATE TABLE movies (
                      movie_id SERIAL PRIMARY KEY NOT NULL,
                      images_path VARCHAR(200) NULL,
                      imdb_score VARCHAR(50)  NULL,
-                     dominant_color_rgb VARCHAR(50)  null,
-                     dominant_color_name VARCHAR(50)  null
-    );''')
+                     dominant_color_rgb VARCHAR(50)  NULL,
+                     dominant_color_name VARCHAR(50)  NULL);''')
 
 
 conn.commit()
@@ -24,12 +23,15 @@ df_movies = pd.read_json('imdb_output.json', orient='columns')
 df_images = df_movies['images']
 #print(df_images[1])
 for idx, m in df_movies.iterrows():
-    paths = m['images'][0]['path']
-    paths = paths[5:]
+    try:
+        paths = m['images'][0]['path']
+        paths = paths[5:]
+    except IndexError:
+        paths = " "
     
     print(paths)
-#cur.execute('''INSERT INTO movies (images_path, imdb_score) VALUES (%s,%s)''', ( m['images'][0]['path'],  m.imdb_score))
-# conn.commit()
+    cur.execute('''INSERT INTO movies (images_path, imdb_score) VALUES (%s,%s)''', ( paths,  m.imdb_score))
+    conn.commit()
 
 cur.close()
 conn.close()
