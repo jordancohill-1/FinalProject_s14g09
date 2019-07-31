4c3fa67c48ee3516e4e3ce3f72af187367ed183d
jordancohill-1 <cohill_jordan@bah.com>
2019-07-31 13:01:14 -0600

    workin csvtopos

--- csvToPos.py
+++ csvToPos.py
@@ -10,7 +10,7 @@ cur.execute("DROP TABLE IF EXISTS colors;")
 
 cur.execute('''CREATE TABLE colors (
                      color_id SERIAL PRIMARY KEY NOT NULL,
-                     movie SERIAL NOT NULL,
+                     movie INT NOT NULL,
                      dominant_color_rgb TEXT NOT NULL,
                      dominant_color_name TEXT NOT NULL,
                      FOREIGN KEY (movie) REFERENCES movies(movie_id)
@@ -22,9 +22,14 @@ conn.commit()
 
 df_colors = pd.read_csv('dominantColors.csv', index_col=0)
 for idx, c in df_colors.iterrows():
-	if cur.execute("""SELECT EXISTS(SELECT movie_id FROM movies WHERE images_path = %s)""" , (c.filename,)):
+	cur.execute("""(SELECT movie_id FROM movies WHERE images_path = %s)""", (c.filename,))
+	mid = cur.fetchone();
+	if mid is not None:
+		mid =int(mid[0])
+		print(mid)
 		cur.execute("""INSERT INTO colors ( movie, dominant_color_rgb, dominant_color_name) VALUES (%s,%s,%s)""", ( 
-			cur.execute("""SELECT movie_id FROM movies WHERE images_path = %s""" , (c.filename,)), c.dominant_color_rgb, c.dominant_color_name))
+			mid, c.dominant_color_rgb, c.dominant_color_name))
+		conn.commit()
 
 
 cur.close()
