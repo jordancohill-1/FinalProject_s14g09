2c059c93ad4bc1a13a819a2faa8a01194353fb83
jordancohill-1 <cohill_jordan@bah.com>
2019-08-03 11:29:10 -0600

    model revision

--- website/models.py
+++ website/models.py
@@ -12,6 +12,6 @@ class Movie(db.Model):
 class Color(db.Model):
     __tablename__ = 'colors'
     color_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    movie = db.Column(db.Integer, ​db.ForeignKey('movies.movie_id'), nullable=False)
+    movie = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
     imdb_score = db.Column(db.Float, nullable=False)
     
