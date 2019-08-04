00abc508ba2513a60fbc5614c3f94935d2d9e504
DonutDeflector <donutdeflector@protonmail.com>
2019-08-04 15:06:05 -0400

    load d3 files upon content replaced

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,10 +1,24 @@
 const swup = new Swup();
 
+// find navbar toggler and menu
 const navbarToggler = document.getElementsByClassName('navbar-toggler')
 const navbarMenu = document.querySelector('.navbar-collapse')
 
+// toggle mobile menu on page change
 document.addEventListener('swup:clickLink', event => {
   if (screen.width < 576 && navbarMenu.classList.contains('show')) {
     navbarToggler[0].click()
+  }
+});
+
+// load d3 scripts when trends page is loaded
+// set cache to true in production
+document.addEventListener('swup:contentReplaced', function () {
+  if (window.location.pathname == '/trends') {
+    $.getScript({url: 'https://d3js.org/d3.v5.min.js', cache: false}, function () {
+      $.getScript({url: '/static/js/barChart.js', cache: false});
+      $.getScript({url: '/static/js/moviePlot1.js', cache: false});
+      $.getScript({url: '/static/js/sunburstChart.js', cache: false});
+    });
   }
 });
